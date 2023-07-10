77fcdad76186fbf65cbfb8fae44d53837ad91fc3
Lusifer <topsale@vip.qq.com>
2023-07-11 00:20:14 +0800

    update

---
+++ src/main/java/com/lusifer/hikvision/HikVisionApplication.java
@@ -0,0 +1,13 @@
+package com.lusifer.hikvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikVisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HikVisionApplication.class, args);
+    }
+
+}
